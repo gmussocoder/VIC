6422674b3ebb882f6b729e0b73aa388360d72611
gmussocoder <musso.guillermo.capacitacion@gmail.com>
2023-04-29 02:02:16 -0300

    Test de Generación de JobId

--- TMService/app.js
+++ TMService/app.js
@@ -11,13 +11,37 @@ app.use(bodyParser.json());
 app.post('/execute-python-script', (req, res) => {
   // Get the script and arguments from the request body
   const { scriptPath, args } = req.body;
+  const jobId = generate_job_id();
 
   // Spawn a Python process with the script path and arguments
   const activate = "C:\\Guille\\VIC\\Desarrollo\\pythonEnv\\Scripts\\activate.bat";
-  const script = "C:\\Guille\\VIC\\Desarrollo\\pythonProcessTest.py";  
+  const script = "C:\\Guille\\VIC\\Desarrollo\\pythonProcessTest.py";
 
-//  const pythonProcess = spawn('python', ['script.py']);
-  const pythonProcess = spawn("cmd.exe", ["/c", activate + " && python", script]);
+  // Set the response content type to text/plain
+  //res.setHeader('Content-Type', 'text/plain');
+
+  // Set the response headers
+  res.set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  });
+  const response = {
+    jobId: {
+      Model: 'modelID',
+      status: 'Training',
+      code: 0,
+      id: jobId
+    }
+  };
+  // Send the JSON response
+  res.send(JSON.stringify(response));
+
+//  res.send(`Modelo entrenándose. JobId: ${jobId}`);
+
+
+  const pythonProcess = spawn("cmd.exe", ["/c", activate + " && python", script, jobId]);
 //  const pythonProcess = spawn('cmd.exe', ['/c', 'activate.bat', '&&', 'python', 'script2.py', 10], { shell: true });
   // Listen for the data event to receive output from the script
   pythonProcess.stdout.on('data', (data) => {
@@ -36,10 +60,28 @@ app.post('/execute-python-script', (req, res) => {
   // Listen for the exit event to handle the process exit status
   pythonProcess.on('exit', (code) => {
     console.log(`Python script exited with code ${code}`);
-    res.send(`Python script exited with code ${code} y la salida del script es ${output}`);
+//    res.send(`Python script exited with code ${code} y la salida del script es ${output}`);
   });
 });
 
 app.listen(port, () => {
   console.log(`API listening at http://localhost:${port}`);
 });
+
+
+//utils
+function generate_job_id() {
+    // Define the length of the job ID
+    const length = 10;
+  
+    // Define the character set to choose from
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  
+    // Generate the random job ID
+    let job_id = '';
+    for (let i = 0; i < length; i++) {
+      job_id += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+  
+    return job_id;
+  }
--- pythonProcessTest.py
+++ pythonProcessTest.py
@@ -1,4 +1,10 @@
 import tensorflow as tf
+import os
+import sys
+
+arg1 = sys.argv
+
+
 
 # Define a simple TensorFlow computation graph
 a = tf.constant(5)
@@ -8,3 +14,14 @@ c = tf.multiply(a, b)
 # Run the computation graph using TensorFlow 2.x API
 result = c.numpy()
 print("Result: {}".format(result))
+
+# Open a file for writing
+filename = "example.txt"
+path = "C:\\Guille\\VIC\\Desarrollo\\pythonEnv"
+filepath = os.path.join(path, filename)
+with open(filepath, "w") as file:
+    file.write(f'arg1: {arg1}\n')
+
+# Print a message to indicate the file has been created
+print(f"File {filename} created at {path}")
+
