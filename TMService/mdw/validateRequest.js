f74c2810a546d1c77bc810f8e121c04d115235b0
gmussocoder <musso.guillermo.capacitacion@gmail.com>
2023-05-01 23:20:38 -0300

    01052023

--- TMService/mdw/validateRequest.js
+++ TMService/mdw/validateRequest.js
@@ -1,13 +1,12 @@
 // The following function Validates that the request is correct.
 function validateRequest(request, response, next) {
     const { modelId, hyperparameters, dataset } = request.body;
+    if (!request.headers['content-type'] || request.headers['content-type'] !== 'application/json') {
+      return response.status(415).json({ error: 'Unsupported Media Type: JSON expected' });
+    };
     if (!modelId || !hyperparameters || !dataset) {
       return response.status(400).json({ error: 'Bad Request: Missing parameters' });
     }
-    if (request.headers['content-type'] !== 'application/json') {
-      return response.status(415).json({ error: 'Unsupported Media Type: JSON expected' });
-    }
     next();
 };
-  
 module.exports = { validateRequest };
