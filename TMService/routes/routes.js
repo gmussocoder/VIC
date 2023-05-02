5c2edbd9001e46fd1277801dd1459cd1cf7ddbb6
gmussocoder <musso.guillermo.capacitacion@gmail.com>
2023-05-02 08:33:59 -0300

    02052023

--- TMService/routes/routes.js
+++ TMService/routes/routes.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const Service = require('../controllers/controller');
 const validateRequest = require('../mdw/validateRequest');
-router.post('/execute-python-script', validateRequest.validateRequest, Service.Service);
+router.post('/TMService/V1/trainModel', validateRequest.validateRequest, Service.Service);
 module.exports = router;
