79aacc4fc38bd0ab79eadf1c65b319982099130c
gmussocoder <musso.guillermo.capacitacion@gmail.com>
2023-05-02 22:26:44 -0300

    02052023

--- pythonProcessTest.py
+++ pythonProcessTest.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 import os
 import sys
+import csv
+import time
+from datetime import datetime
 
 arg1 = sys.argv
 
@@ -14,14 +17,35 @@ c = tf.multiply(a, b)
 # Run the computation graph using TensorFlow 2.x API
 result = c.numpy()
 print("Result: {}".format(result))
-
+'''
 # Open a file for writing
 filename = "example.txt"
 path = "C:\\Guille\\VIC\\Desarrollo\\pythonEnv"
 filepath = os.path.join(path, filename)
 with open(filepath, "w") as file:
     file.write(f'arg1: {arg1}\n')
+'''
+filename = "example.csv"
+path = "C:\\Guille\\VIC\\Desarrollo\\pythonEnv"
+filepath = os.path.join(path, filename)
+
+# If the file doesn't exist, write the header row
+if not os.path.exists(filepath):
+    with open(filepath, "w", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["jobId", "timestamp_start", "timestamp_finish", "result", "code", "description"])
+
+# Open the file in append mode
+with open(filepath, "a", newline='') as file:
+    writer = csv.writer(file)
+    timestamp_start = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3] # current timestamp with milliseconds
+    # Perform your operation here
+    result = "some result"
+    time.sleep(1)
+    code = 200
+    description = "some description"
+    timestamp_finish = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3] # current timestamp with milliseconds
+    writer.writerow(["job1", timestamp_start, timestamp_finish, result, code, description])
 
 # Print a message to indicate the file has been created
 print(f"File {filename} created at {path}")
-
