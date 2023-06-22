b5a76a64c9b1e767f62361bab948b05a351381f4
gmussocoder <musso.guillermo.capacitacion@gmail.com>
2023-06-22 08:49:41 -0300

    22062023

--- AdapterService/routes/routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Service = require('../controllers/JS/controller');
-const validateRequest = require('../mdw/validateRequest');
-router.post('/AdapterService/V1/iresults', validateRequest.validateRequest, Service.Service);
-module.exports = router;
